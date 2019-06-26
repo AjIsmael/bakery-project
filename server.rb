7a2f4b5efa257b0c2e4834f14fd63051ab94760c
Ajaeb Ismael <ajaebabdurahman@outlook.com>
2019-06-26 13:03:51 -0400

    created class layout and files that are layout for the start of the project

---
+++ server.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+
+class Cookie
+  def initialize(name, calories, type, price)
+    @name = name
+  end
+end
+
+class Muffin
+  def initialize(name,calories, type, price)
+    @name = name
+  end
+end
+
+class Cake
+  def initialize(calories, type, price)
+    @name = name
+  end
+end
