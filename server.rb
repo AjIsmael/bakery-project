50fed24db000fbb270927c92252afb6deaf0f037
Ajaeb Ismael <ajaebabdurahman@outlook.com>
2019-06-27 14:32:21 -0400

    added an event and cart the cart needs work.

--- server.rb
+++ server.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
+require "httparty"
 
+app_key = 'AVPKBRW3EJMHUBKBT347'
+
+Orders = []
 class Cookie
   attr_accessor :name, :calories, :description, :price, :image
   def initialize(name, calories, description, price, image)
@@ -38,12 +42,12 @@ peanut_butter_cookie = Cookie.new("Peanut Butter Cookie", "135 calories per cook
 rasperry_cookie = Cookie.new("Rasperry Cookie", "120 calories per cookie", "Thumbprint cookies with a buttery base, filled with raspberry preserves","1.60", "https://friendly-bakery-nycda.herokuapp.com/images/cookies/raspberry-cookies.jpg")
 
 lemon_meringue_cake = Cake.new("Lemon Meringue Cake", "170 calories per slice", "It's not only a deliciously different dessert, but it's also a conversation piece!", "4.50", "https://friendly-bakery-nycda.herokuapp.com/images/cakes/lemon-meringue-cake.jpg")
-vanilla_cake = Cake.new("Vanilla Cake with Blueberries", "170 calories per slice", "pairs fluffy vanilla cake layers with a silky vanilla buttercream. the perfect cake for birthdays, weddings, or any occasion!", "4.50", "https://friendly-bakery-nycda.herokuapp.com/images/cakes/vanilla-white-cake.jpg")
+vanilla_cake = Cake.new("Vanilla Cake with Blueberries", "170 calories per slice", "pairs fluffy vanilla cake layers with a silky vanilla buttercream.", "4.50", "https://friendly-bakery-nycda.herokuapp.com/images/cakes/vanilla-white-cake.jpg")
 neapolitan_ice_cream = Cake.new("Neapolitan Ice Cream","180 calories per slice","tasty combination of chocolate, strawberry, and vanilla ice cream. ", "4.50", "https://friendly-bakery-nycda.herokuapp.com/images/cakes/neapolitan-ice-cream-cake.jpg")
 
-apple_pie_muffins = Muffin.new("Apple Pie Muffins", "180 calories per muffin","These buttery fruity muffins are ideal for dessert or fancy weekend breakfast treats.", "2.70", "https://friendly-bakery-nycda.herokuapp.com/images/muffins/apple-pie-muffins.jpg")
-coffee_cake_muffins = Muffin.new("Coffee Cake Muffins","180 calories per muffin","Coffee Cake Muffins, with its sweet center and crumble topping, make a great dish to serve at a brunch, or bring to a potluck.","2.70","https://friendly-bakery-nycda.herokuapp.com/images/muffins/coconut-muffins.jpg")
-coconut_muffins = Muffin.new("Coconut Muffins", "180 calories per muffin", "Muffins loaded with coconut are one my favorite muffins for an easy breakfast or a sweet treat late at night.", "2.70", "https://friendly-bakery-nycda.herokuapp.com/images/muffins/coconut-muffins.jpg")
+apple_pie_muffins = Muffin.new("Apple Pie Muffins", "180 calories per muffin","These buttery fruity muffins are ideal for dessert or fancy breakfast treats.", "2.70", "https://friendly-bakery-nycda.herokuapp.com/images/muffins/apple-pie-muffins.jpg")
+coffee_cake_muffins = Muffin.new("Coffee Cake Muffins","180 calories per muffin","Its sweet center and crumble topping, is great dish to serve at a brunch.","2.70","https://friendly-bakery-nycda.herokuapp.com/images/muffins/coconut-muffins.jpg")
+coconut_muffins = Muffin.new("Coconut Muffins", "180 calories per muffin", "Loaded with coconut are for an easy breakfast or a sweet treat late at night.", "2.70", "https://friendly-bakery-nycda.herokuapp.com/images/muffins/coconut-muffins.jpg")
 
 
 
@@ -58,6 +62,9 @@ end
 
 get "/cookies" do
   @cookies = [chocolate_chip_cookie, peanut_butter_cookie, rasperry_cookie]
+  if params[:cookie0].to_i > 0
+    Orders << [chocolate_chip_cookie.name, params[:cookie0],chocolate_chip_cookie.price]
+  end
   erb :cookies
 end
 get "/muffins" do
@@ -66,6 +73,25 @@ get "/muffins" do
 end
 
 get "/events" do
+  response = HTTParty.get("https://www.eventbriteapi.com/v3/events/search/?q=baking&location.address=#{params[:address]}&price=free&token=#{app_key}", format: :plain)
+  jsonFile = JSON.parse response, symbolize_names: true
+  events = jsonFile.dig(:events)
+  eventtitles = []
+  describtion = []
+  start_date = []
+  image = []
+  1..3.times do |i|
+    event = events[i]
+    eventtitles << event.dig(:name,:text)
+    describtion << event.dig(:url)
+    start_date << event.dig(:start,:local).split('T')[0]
+    image << event.dig(:logo,:original, :url)
+  end
+
+  @titles = eventtitles
+  @description=  describtion
+  @date = start_date
+  @image = image
   erb :events
 end
 
@@ -74,5 +100,6 @@ get "/home" do
 end
 
 get "/cart" do
+  @order = Orders
   erb :cart
 end
